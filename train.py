b52fdd272d427a1a26d3398c25222f938f176b24
MicheleCeresoli <michele.ceresoli@polimi.it>
2025-07-18 18:06:52 +0200

    Updated training script

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from elope.trainers import LunarTrainer
 from elope.utils import LOGGER, increment_path
 
 # Path to the yaml file containing the dataset settings
-DATASET_CFG = "cfg/dataset/rng-5s.yml"
+DATASET_CFG = "cfg/dataset/dataset-5s.yml"
 
 # Path to the yaml file containing the model settings
 MODEL_CFG = "cfg/training/emmnet-v1.yml"
@@ -24,24 +24,29 @@ all_sequences = [str(i).zfill(4) for i in range(28)]
 seq_train = all_sequences[:22]  # 80% for training
 seq_val = all_sequences[22:]    # 20% for validation
 
-# Create/load the training and validation datasets
-train_dataset = ElopeDataset.from_yaml(DATASET_CFG, seq_train, mode="train")
-val_dataset = ElopeDataset.from_yaml(DATASET_CFG, seq_val, mode="val")
-
 # Create the PyTorch's dataloaders
 train_loader = ElopeDataLoader(
-    train_dataset, batch_size=train_dataset.batch_size, shuffle=True, num_workers=8
+    DATASET_CFG,
+    seq_train, 
+    augment=True, 
+    batch_size=32,
+    shuffle=True, 
+    num_workers=8
 )
 
 val_loader = ElopeDataLoader(
-    val_dataset, batch_size=val_dataset.batch_size, shuffle=True, num_workers=4
+    DATASET_CFG, 
+    seq_val, 
+    augment=False,
+    batch_size=32, 
+    shuffle=True, 
+    num_workers=4
 )
 
 # Create the network model 
 model = MultiModalVelocityEstimator.create_model(MODEL_CFG, device=device)
 
 # Create the trainer for the model 
-# TODO: add configuration for the loss function
 trainer = LunarTrainer(MODEL_CFG, model, train_loader, val_loader, device)
 
 # Train the model 
