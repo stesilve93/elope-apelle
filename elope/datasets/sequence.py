21f96145dfb7326912f43e65d18d492783b446fe
MicheleCeresoli <michele.ceresoli@polimi.it>
2025-07-21 15:09:33 +0000

    Fixed sequence generation

--- elope/datasets/sequence.py
+++ elope/datasets/sequence.py
@@ -66,6 +66,11 @@ class SequenceLoader:
         self.W = int(event_W)
         self.T = int(event_T)
         
+        # Ensure the encoder method is supported
+        assert self.event_encoder_method in (
+            "first_timestamp", "last_timestamp", "timestamp", "count", "hybrid"
+        )
+        
         # Ensure the number of time bins is coherent with the encoder method 
         if self.event_encoder_method in ("first_timestamp", "last_timestamp") and self.T != 1: 
             raise ValueError(
@@ -349,12 +354,7 @@ class SequenceLoader:
         if events_array.shape[0] == 0: 
             # LOGGER.warning("No events found in the specified time window.")
             # Return an empty tensor with the expected shape 
-            if self.event_encoder_method == "count": 
-                return np.zeros((2, T, H, W), dtype=np.float32)
-            elif self.event_encoder_method == "last_timestamp": 
-                return np.zeros((2, 1, H, W), dtype=np.float32)
-            else: 
-                raise ValueError("Unsupported event encoding method")
+            return np.zeros((2, T, H, W), dtype=np.float32)
         
         # Convert the filtered events into a 4D tensor 
         tensor = self.processor.events_to_tensor(
