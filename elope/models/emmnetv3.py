0be7f8021f771a493133fb68ffd6c154d9a8bd9c
MicheleCeresoli <michele.ceresoli@polimi.it>
2025-07-24 11:52:14 +0200

    New arch WIP

--- elope/models/emmnetv3.py
+++ elope/models/emmnetv3.py
@@ -10,41 +10,114 @@ from pathlib import Path
 
 from elope.utils import load_yaml
 
-class BaseMLP(nn.Module): 
+class BaseEmbedding(nn.Module): 
     """Base MLP layer for basic feature expansion.""" 
     
     def __init__(
         self, 
         input_dim: int = 2,
-        hidden_dim: int = 32,
-        num_layers: int = 1,
+        hidden_dim: int | list = 32,
         activation: nn.Module = nn.ReLU(), 
-        norm: bool=True,
+        norm: bool = False,
         dropout: float = 0.1, 
-    )
+    ):
         # Initialize the base class 
         super().__init__() 
         
-        c1, c2 = input_dim, hidden_dim
+        # Ensure it is a lsit
+        if isinstance(hidden_dim, int): 
+            hidden_dim = [hidden_dim]
+        
+        # Retrieve number of layers 
+        num_layers = len(hidden_dim)
+        
+        # Retrieve dimensions of first FC layer
+        c1 = input_dim
         
         # Add FC layer
         self.layers = []
-        for _ in num_layers: 
+        for k in range(num_layers): 
             
-            layer = nn.Module(
-                nn.Linear()
+            # Update the channel dimension 
+            c2 = hidden_dim[k] 
+            
+            self.layers.append(
+                nn.Sequential(
+                    nn.Linear(c1, c2), 
+                    activation, 
+                    # Layer Norm is deactivated by default because when inputs consists 
+                    # of difference physical units we would be losing that. 
+                    nn.LayerNorm(c2) if norm else nn.Identity(), 
+                    nn.Dropout(dropout) if dropout > 0 else nn.Identity()
+                )
             )
             
-             
-        self.layer = nn.Linear(input_dim, hidden_dim)
-        
-        # Add the activation function 
-        self.act = activation
-        
-        # Create dropout and layer norms
-        self.bn1 = nn.LayerNorm(hidden_dim) if norm else nn.Identity()
-        self.dropout = nn.Dropout(dropout) if dropout > 0 else nn.Identity() 
+            # Update the next layer input dims
+            c1 = c2
     
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x is a tensor of shape (B, S, N)
+        for layer in self.layers: 
+            x = layer(x)
+        
+        return x
+    
+class VelocityRegressor(nn.Module): 
+    
+    def __init__(
+        self, 
+        input_dim: int, 
+        output_dim: int,
+        hidden_dim: int | list = [128, 64],
+        activation: nn.Module = nn.GELU(),
+        norm: bool = False, 
+        dropout: float = 0.0, 
+    ): 
+        super().__init__()
+        
+        # Ensure the hidden dim is a list 
+        if isinstance(hidden_dim, int): 
+            hidden_dim = [hidden_dim]
+            
+        # Retrieve the number of layers 
+        num_layers = len(hidden_dim)
+        
+        # Retrieve the dimenions of the first FC layer 
+        c1 = input_dim
+        
+        # Add the FC layers 
+        self.layers = []
+        for k in range(num_layers): 
+            
+            # Update the channel dimension 
+            c2 = hidden_dim[k]
+            
+            self.layers.append(
+                nn.Sequential(
+                    nn.Linear(c1, c2), 
+                    activation, 
+                    nn.LayerNorm(c2) if norm else nn.Identity(),
+                    nn.Dropout(dropout) if dropout > 0 else nn.Identity() 
+                )
+            )
+            
+            # Update the next layer input dims
+            c1 = c2 
+        
+        # Add a final fully connected layer 
+        # TODO: evaluate whether this can be improved by adding skip connections
+        self.proj_head = nn.Linear(c2, output_dim)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor: 
+        
+        # Apply the different layers
+        for layer in self.layers: 
+            x = layer(x)
+        
+        # Final layer (without activation for the regression)
+        out = self.proj_head(x)
+        return out
+        
 
 class MultiModalTransformerEstimator(nn.Module):
     """ Multi-modal transformer-based network with better regularization."""
@@ -55,6 +128,33 @@ class MultiModalTransformerEstimator(nn.Module):
     ):
 
         super().__init__()
+        
+        # Initialise the BaseEmbedding block for the rangemeter data 
+        self.proj_range = BaseEmbedding(
+            input_dim = 2, 
+            hidden_dim = [32, 16], 
+            activation = nn.SiLU(),
+            norm = False, 
+            dropout = dropout
+        )
+        
+        # Initialise the BaseEmbedding block for the IMU data 
+        self.proj_imu = BaseEmbedding(
+            input_dim = 7, 
+            hidden_dim = [64, 64, 32], 
+            activation = nn.SiLU(),
+            norm = False, 
+            dropout = dropout
+        )
+        
+        self.regressor_head = VelocityRegressor(
+            input_dim = , # TODO: add dimension of transfomer output.
+            output_dim = 3,
+            hidden_dim=[128, 64]
+            activation=nn.SiLU(), 
+            norm=False, 
+            dropout = dropout,
+        )
 
         # Initialize weights to prevent overfitting
         self._init_weights()
@@ -88,6 +188,7 @@ class MultiModalTransformerEstimator(nn.Module):
                 if module.bias is not None:
                     # Initialize bias to zero
                     nn.init.constant_(module.bias, 0)
+                    
             elif isinstance(module, (nn.Conv3d, nn.Conv2d, nn.Conv1d)):
                 # Initialize weights using kaiming_normal initialization
                 nn.init.kaiming_normal_(module.weight, mode='fan_out', nonlinearity='relu')
@@ -118,12 +219,18 @@ class MultiModalTransformerEstimator(nn.Module):
         # Stack the timestep to the IMU data 
         tensor_ext_imu = torch.cat((tensor_imu, time_step)) # (B, S, 7)
         
-        # TODO: add rangemeter embedding 
-        # TODO: add IMU embedding 
+        # Compute the range embeddings
+        feat_range = self.proj_range(tensor_ext_range) # (B, S, 16)
+        
+        # Compute the IMU embeddings 
+        feat_imu = self.proj_imu(tensor_ext_imu) # (B, S, 32)
+        
         # TODO: add event-frame encoder 
         
         # TODO: add transformer fuser 
+        
         # TODO: add final regressor
+        output= self.regressor_head()
         
         return {
             'prediction': output,
