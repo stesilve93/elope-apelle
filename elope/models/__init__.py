457ae76f043619a35ab1029ca11131aa177ecf0e
MicheleCeresoli <michele.ceresoli@polimi.it>
2025-07-28 19:08:00 +0200

    Updated model build entry

--- elope/models/__init__.py
+++ elope/models/__init__.py
@@ -13,27 +13,44 @@ def build_model(
     # Retrieve number of event channels per polarity
     event_channels = int(cfg_dataset["events"]["channels"])
     
-    # Lets check what kind of model we are dealing with 
-    if "architecture" in cfg_model.keys(): 
-        # SETNet type 
-        return SETNetV1.create_model(
-            cfg_model, 
-            event_channels=event_channels, 
-            device=device
-        )
+    # Retrieve the model architecture
+    model = cfg_model["model"]
+    
+    if "emmnet" in model: 
+        # EMMNET-models
         
-    # EMMNET type
-    if cfg_model["output_type"] == "sequence": 
-        return MultiModalVelocityEstimatorS2S.create_model(cfg_model, device)
-    else: 
+        if model == "emmnet-base": 
+            return MultiModalVelocityEstimator.create_model(cfg_model, device)
         
-        if cfg_model.get("use_nopool", False): 
+        elif model == "emmnet-nopool": 
             return MultiModalVelocityEstimatorNoPool.create_model(
                 cfg_model, 
-                event_channels=event_channels,
-                device=device,
+                event_channels=event_channels, 
+                device=device
             )
+            
+        elif model == "emmnet-seq2seq": 
+            assert cfg_model["output_type"] == "sequence"
+            return MultiModalVelocityEstimatorS2S.create_model(cfg_model, device)
+        
+        elif model == "emmnet-transformer": 
+            return MultiModalVelocityEstimatorTransformer.create_model(cfg_model, device)
         
         else: 
-            return MultiModalVelocityEstimator.create_model(cfg_model, device)
-
+            raise ValueError(f"Unsupported emmnet model: {model}")
+    
+    elif "setnet" in model:
+        # SETNET-models 
+        
+        if model == "setnet-v1": 
+            return SETNetV1.create_model(
+                cfg_model, 
+                event_channels=event_channels, 
+                device=device
+            )
+            
+        else: 
+            raise ValueError(f"Unsupported setnet model: {model}")
+    
+    else: 
+        raise ValueError(f"Unrecognised network model: {model}")
